b31e683664034427594bc871468d4f3d021dd020
mnyellison <nmatheus.moura@hotmail.com>
2026-06-07 13:57:31 -0300

    feat: complete article preview component with responsive design and interactive share menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ buttonShare.addEventListener("click", () => {
   buttonShare.classList.toggle("active");
   shareContainer.classList.toggle("hidden");
 });
+
+document.addEventListener("click", (event) => {
+  if (!shareContainer.classList.contains("hidden")) {
+    if (
+      !shareContainer.contains(event.target) &&
+      !buttonShare.contains(event.target)
+    ) {
+      shareContainer.classList.add("hidden");
+      buttonShare.classList.remove("active");
+    }
+  }
+});
